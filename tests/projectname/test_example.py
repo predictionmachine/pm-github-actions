31b7148a09ad8028d7456dfc7c39a9f278589b67
Timothy Chklovski <tchklovski@gmail.com>
2021-06-15 17:34:39 -0700

    try mypy

--- tests/projectname/test_example.py
+++ tests/projectname/test_example.py
@@ -6,5 +6,5 @@ from projectname.example import UserId, listify
 
 
 @pytest.mark.parametrize("user_id, expected_val", [(UserId(1), [UserId(1)])])
-def test_listify(user_id: UserId, expected_val: List[UserId]) -> None:
+def test_listify(user_id: UserId, expected_val: List[UserId]):
     assert listify(user_id) == expected_val
